1d9f5a7f471a18364f77aae7c4f12b85017b7f88
Artem <arty.lindemann@gmail.com>
2023-01-21 20:18:26 +0600

    Initial commit

---
+++ auth.php
@@ -0,0 +1,23 @@
+<?php
+	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+	$pass = md5($pass."asvnmdfg92345");
+
+	$mysql = new mysqli('localhost', 'admin', '12345', 'register-bd');
+
+
+	$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'");
+	$user = $result->fetch_assoc();
+	if(count($user) == 0){
+		echo "The user does not exist";
+		exit();
+	}
+
+	setcookie('user', $user['name'], time() + 3600, "/");
+
+
+	$mysql->close();
+
+	header('Location: /activebox.com');
+?>
---
+++ check.php
@@ -0,0 +1,25 @@
+<?php
+	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+	$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+	if(mb_strlen($login) < 5 || mb_strlen($login) > 90) {
+		echo "Invalid login length";
+		exit();
+	} else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+		echo "Invalid name length";
+		exit();
+	} else if(mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
+		echo "Invalid password length (from 2 to 6 symbols)";
+		exit();
+	}
+	
+	$pass = md5($pass."asvnmdfg92345");
+
+	$mysql = new mysqli('localhost', 'admin', '12345', 'register-bd');
+	$mysql->query("INSERT INTO `users` (`login`, `pass`, `name`)
+	VALUES('$login', '$pass', '$name')");
+	$mysql->close();
+
+	header('Location: /activebox.com');
+?>
